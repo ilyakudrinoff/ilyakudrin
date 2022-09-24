d003db71d0994cf0a6ee2cb2e1933b58db5f4c0e
ilya <ilyakudrinofficial@mail.ru>
2022-09-24 12:33:26 +0700

    wsgi файл

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+sys.path.insert(0, '/var/www/u1793832/data/www/kudrinofficial.ru/ilyakudrin')
+sys.path.insert(1, '/var/www/u1793832/data/venv/lib/python3.8/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ilyakudrin.config.settings'
 
 application = get_wsgi_application()
