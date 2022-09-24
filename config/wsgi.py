45ac6b4776c0168b8db87d62515b492803a91cb3
ilya <ilyakudrinofficial@mail.ru>
2022-09-24 12:51:11 +0700

    f

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,8 +3,8 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, '/var/www/u1793832/data/www/kudrinofficial.ru/ilyakudrin')
+sys.path.insert(0, '/var/www/u1793832/data/www/kudrinofficial.ru/')
 sys.path.insert(1, '/var/www/u1793832/data/venv/lib/python3.8/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ilyakudrin.config.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 
 application = get_wsgi_application()
